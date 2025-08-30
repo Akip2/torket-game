254923468ab352a11eebe87cdeef1694b43e85e1
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-08-30 16:43:34 +0200

    Player joining and leaving room colyseus

--- server/src/rooms/MyRoom.ts
+++ server/src/rooms/MyRoom.ts
@@ -1,5 +1,5 @@
 import { Room, Client } from "@colyseus/core";
-import { MyRoomState } from "./schema/MyRoomState";
+import { MyRoomState, Player } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
   maxClients = 4;
@@ -15,10 +15,18 @@ export class MyRoom extends Room<MyRoomState> {
 
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
+
+    const player = new Player();
+    player.x = 0;
+    player.y = 0;
+
+    this.state.players.set(client.sessionId, player);
   }
 
   onLeave (client: Client, consented: boolean) {
     console.log(client.sessionId, "left!");
+
+    this.state.players.delete(client.sessionId);
   }
 
   onDispose() {
