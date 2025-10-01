357682eb3dc6499576efa84eb135b191476bbc9b
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-10-01 23:57:29 +0200

    Projectile explodes when touching player

--- shared/logics/bullet-logic.ts
+++ shared/logics/bullet-logic.ts
@@ -1,3 +1,4 @@
+import { PLAYER_CONST } from "@shared/const";
 import Vector from "../data/Vector";
 import type { IBasicBody } from "../interfaces/BasicBody.interface"
 
@@ -12,4 +13,19 @@ export function shoot(bullet: IBasicBody, x: number, y: number, force: number) {
     const normalizedBulletVector = bulletVector.getNormalizedVector();
 
     bullet.setVelocity(normalizedBulletVector.x * (force), normalizedBulletVector.y * force);
+}
+
+export function generateBulletOriginPosition(playerX: number, playerY: number, targetX: number, targetY: number, playerSize: number = PLAYER_CONST.WIDTH) {
+    const bulletVector = new Vector(
+        targetX - playerX,
+        targetY - playerY
+    );
+
+    const normalizedBulletVector = bulletVector.getNormalizedVector();
+    const distance = playerSize / 2;
+
+    return {
+        x: playerX + normalizedBulletVector.x * distance,
+        y: playerY + normalizedBulletVector.y * distance
+    }
 }
