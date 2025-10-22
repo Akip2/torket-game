1f92af1f7f76c0c6199da01f652647beda9cad9f
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-10-23 01:49:37 +0200

    Changing size of brush in map editor

--- client/src/scenes/MapEditionScene.ts
+++ client/src/scenes/MapEditionScene.ts
@@ -1,16 +1,16 @@
 import { GAME_HEIGHT, GAME_WIDTH, TEXTURE_SIZE, TILE_SIZE } from "@shared/const";
-import PrimitiveMap from "@shared/data/PrimitiveMap"
+import PrimitiveMap from "@shared/data/PrimitiveMap";
 import { RessourceKeys } from "@shared/enums/RessourceKeys.enum";
 
 export default class MapEditionScene extends Phaser.Scene {
     currentMap: PrimitiveMap;
-    tiles: Phaser.GameObjects.TileSprite[] = []
+    tiles: Phaser.GameObjects.TileSprite[] = [];
 
-    isPointerDown: boolean = false;
+    brushSize: number = 2; // brush and eraser size in tiles
+    brushPreview!: Phaser.GameObjects.Rectangle;
 
     constructor() {
-        super('MapEditor');
-
+        super("MapEditor");
         this.currentMap = new PrimitiveMap(GAME_WIDTH, GAME_HEIGHT, TILE_SIZE);
     }
 
@@ -19,49 +19,91 @@ export default class MapEditionScene extends Phaser.Scene {
     }
 
     create() {
-        this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => this.pointerDownEvent(pointer));
-        this.input.on('pointerup', () => this.pointerUpEvent());
-        this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => this.pointerMoveEvent(pointer));
+        this.input.on("pointerdown", (pointer: Phaser.Input.Pointer) => this.doToolAction(pointer));
+        this.input.on("pointermove", (pointer: Phaser.Input.Pointer) => {
+            this.doToolAction(pointer);
+            this.updateBrushPreview(pointer);
+        });
+        this.input.mouse?.disableContextMenu();
+
+        this.input.keyboard!.on("keydown-ONE", () => (this.brushSize = 1));
+        this.input.keyboard!.on("keydown-TWO", () => (this.brushSize = 2));
+        this.input.keyboard!.on("keydown-THREE", () => (this.brushSize = 3));
+        this.input.keyboard!.on("keydown-FOUR", () => (this.brushSize = 4));
+
+        this.brushPreview = this.add.rectangle(0, 0, TILE_SIZE, TILE_SIZE, 0x00ff00, 0.25)
+            .setOrigin(0)
+            .setDepth(1000)
+            .setVisible(false);
     }
 
-    pointerDownEvent(pointer: Phaser.Input.Pointer) {
-        this.isPointerDown = true;
-        this.addTile(pointer.x, pointer.y);
-    }
-
-    pointerUpEvent() {
-        this.isPointerDown = false;
-    }
-
-    pointerMoveEvent(pointer: Phaser.Input.Pointer) {
-        if (this.isPointerDown) {
-            this.addTile(pointer.x, pointer.y);
+    doToolAction(pointer: Phaser.Input.Pointer) {
+        if (pointer.leftButtonDown()) {
+            this.paintTiles(pointer.x, pointer.y);
+        } else if (pointer.rightButtonDown()) {
+            this.eraseTiles(pointer.x, pointer.y);
         }
     }
 
-    addTile(x: number, y: number) {
-        if (this.currentMap.isFilled(x, y)) return;
+    paintTiles(x: number, y: number) {
+        const tileX = Math.floor(x / this.currentMap.minTileSize);
+        const tileY = Math.floor(y / this.currentMap.minTileSize);
 
-        this.currentMap.add(x, y);
+        for (let dy = 0; dy < this.brushSize; dy++) {
+            for (let dx = 0; dx < this.brushSize; dx++) {
+                const px = (tileX + dx) * this.currentMap.minTileSize;
+                const py = (tileY + dy) * this.currentMap.minTileSize;
 
-        const sprite = this.add.tileSprite(
-            Math.floor(x / this.currentMap.minTileSize) * this.currentMap.minTileSize,
-            Math.floor(y / this.currentMap.minTileSize) * this.currentMap.minTileSize,
-            this.currentMap.minTileSize,
-            this.currentMap.minTileSize,
-            RessourceKeys.Ground
-        ).setOrigin(0);
+                if (!this.currentMap.isFilled(px, py)) {
+                    this.currentMap.add(px, py);
 
-        const index = this.currentMap.getIndex(x, y);
-        this.tiles[index] = sprite;
+                    const sprite = this.add.tileSprite(
+                        px,
+                        py,
+                        this.currentMap.minTileSize,
+                        this.currentMap.minTileSize,
+                        RessourceKeys.Ground
+                    ).setOrigin(0);
+
+                    const index = this.currentMap.getIndex(px, py);
+                    this.tiles[index] = sprite;
+                }
+            }
+        }
     }
 
-    removeTile(x: number, y: number) {
-        this.currentMap.remove(x, y);
+    eraseTiles(x: number, y: number) {
+        const tileX = Math.floor(x / this.currentMap.minTileSize);
+        const tileY = Math.floor(y / this.currentMap.minTileSize);
 
-        const index = this.currentMap.getIndex(x, y);
-        const [removedTile] = this.tiles.splice(index, 1);
+        for (let dy = 0; dy < this.brushSize; dy++) {
+            for (let dx = 0; dx < this.brushSize; dx++) {
+                const px = (tileX + dx) * this.currentMap.minTileSize;
+                const py = (tileY + dy) * this.currentMap.minTileSize;
 
-        removedTile.destroy();
+                if (this.currentMap.isFilled(px, py)) {
+                    this.currentMap.remove(px, py);
+
+                    const index = this.currentMap.getIndex(px, py);
+                    const tile = this.tiles[index];
+                    if (tile) {
+                        tile.destroy();
+                        delete this.tiles[index];
+                    };
+                }
+            }
+        }
+    }
+
+    updateBrushPreview(pointer: Phaser.Input.Pointer) {
+        const tileSize = this.currentMap.minTileSize;
+        const snappedX = Math.floor(pointer.x / tileSize) * tileSize;
+        const snappedY = Math.floor(pointer.y / tileSize) * tileSize;
+
+        this.brushPreview
+            .setPosition(snappedX, snappedY)
+            .setSize(tileSize * this.brushSize, tileSize * this.brushSize)
+            .setFillStyle(pointer.leftButtonDown() ? 0x00ff00 : 0xff0000, 0.25)
+            .setVisible(true);
     }
 }
