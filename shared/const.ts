cd788eb2ab36ab33f092376b8c04e8d41d6948d5
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2026-06-09 02:02:37 +0200

    putting player size back to 32

--- shared/const.ts
+++ shared/const.ts
@@ -30,7 +30,7 @@ export const TIME_STEP = 1000 / 60;
 export const HEALTH_TRANSITION_DURATION = 400;
 
 export const PLAYER_CONST = {
-    BASE_WIDTH: 40,
+    BASE_WIDTH: 32,
     SPEED: 4.5,
     JUMP: -18,
     BASE_MAX_HP: 100,
